8953d0376a1a71077ef2263ebd8fe6bc6461a6ba
Ndongmo Christian <christianhonore2003@gmail.com>
2023-01-09 21:15:55 +0100

    fix: Modified a span

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Graping elements from the browser using querySelectors
-const el1 = document.querySelector('#menu .item span');
-const els = document.querySelectorAll('div span');
-
+const el1 = document.querySelector('#menu .item span').innerHTML="<p><strong>christian</strong></p>";;
+const els = document.querySelector('div span')
 // Displaying them in the console
 console.log(el1.innerHTML);
-console.log(els)
+console.log(els.innerHTML)
