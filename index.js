96d391e453910ea98f701f49cb96467ed8635cf0
Ndongmo Christian <christianhonore2003@gmail.com>
2023-01-10 19:17:19 +0100

    feat: Ability to select parent nodes and child nodes from the current selected nodes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Graping elements from the browser using querySelectors
-const el1 = document.querySelector('#menu .item span').innerHTML="<p><strong>christian</strong></p>";;
+const el1 = document.querySelector('#menu .item span').innerHTML="<p><strong>christian</strong></p>";
 const els = document.querySelector('div span')
 // Displaying them in the console
 console.log(el1.innerHTML);
