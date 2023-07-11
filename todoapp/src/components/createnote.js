050383769a2226fce7a5ac99de4a7b53707a7792
Akashiutchiha <christianhonore2003@gmail.com>
2023-07-11 09:36:04 +0100

    feat: Added create note page

---
+++ todoapp/src/components/createnote.js
@@ -0,0 +1,11 @@
+const Create = () => {
+    return ( 
+        <div>
+            <p>Add Note</p>
+            <input type="text" name="" id="" />
+            <button>Add</button>
+        </div>
+     );
+}
+ 
+export default Create;
