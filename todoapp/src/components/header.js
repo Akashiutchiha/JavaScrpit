e02d0ee4c05edb973ef5c9f88e7f07d820158e23
Akashiutchiha <christianhonore2003@gmail.com>
2023-07-11 08:34:07 +0100

    feat:Completed header

---
+++ todoapp/src/components/header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return ( 
+        <div className="header">
+            <p>Todo App</p>
+            <p><i class="plus bi bi-plus-circle"></i></p>
+        </div>
+     );
+}
+ 
+export default Header;
